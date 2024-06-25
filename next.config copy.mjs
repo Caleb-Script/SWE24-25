f0c97a05141987e43a52473105cc0b343e8e5f12
Caleb-Script <caleb-script@outlook.de>
2024-06-25 13:35:35 +0200

    initial

---
+++ next.config copy.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+
+const nextConfig = {
+  images: {
+    domains: ["mdbcdn.b-cdn.net"], // Füge den Hostname hier hinzu
+  },
+};
+
+export default nextConfig;
